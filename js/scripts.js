dda1a50d684b24bd2c003862d99e559d28aaf5d4
Brady <bradklockenbiz@gmail.com>
2019-03-29 11:29:19 -0700

    Add intial working js/jQuery, language choice for c#, custom css style sheets.

--- js/scripts.js
+++ js/scripts.js
@@ -1,50 +1,17 @@
 $(document).ready(function() {
   $("form#languagematch").submit(function(event) {
-    var languagechoice = parseInt($("input#languagechoice").val());
-    var picjb = "img/justin.jpg";
+    var languagechoice = ($("select#languagechoice").val());
+    var piccsharp = "img/csharp.jpg";
     var picag = "img/ariana.jpg"
     var picts = "img/taylor.jpg"
     var picrg = "img/ryan.jpg"
     var picgg = "img/golum.jpg"
 
 
-    if (languagechoice === 'c#' && age <= 21 ) {
+    if (languagechoice === 'c#' && 'c#' && 'c#' && 'c#' && 'c#') {
 
-    $("#matchpic").append("<img src=" + picjb + ">");
-    $("#soulmate").empty().append("Justin Bieber");
-    $("#result").show();
-
-    } else if (languagechoice === 'male' && age <= 21) {
-
-    $("#matchpic").append("<img src=" + picag + ">");
-    $("#soulmate").empty().append("Ariana Grande");
-    $("#result").show();
-
-
-    } else if (languagechoice === 'male' && age <= 40) {
-
-    $("#matchpic").append("<img src=" + picts + ">");
-    $("#soulmate").empty().append("Taylor Swift");
-    $("#result").show();
-    }
-
-    else if (languagechoice === 'female' && age <= 30) {
-
-    $("#matchpic").append("<img src=" + picrg + ">");
-    $("#soulmate").empty().append("Ryan Gosling");
-    $("#result").show();
-    }
-
-    else if (languagechoice === 'female' && age > 30) {
-
-    $("#matchpic").append("<img src=" + picgg + ">");
-    $("#soulmate").empty().append("Golum");
-    $("#result").show();
-
-    } else if (languagechoice === 'male' && age > 30) {
-
-    $("#matchpic").append("<img src=" + picgg + ">");
-    $("#soulmate").empty().append("Golum");
+    $("#languagepic").append("<img src=" + piccsharp + ">");
+    $("#language").empty().append("C#");
     $("#result").show();
 
     } else {
